559024c8ceb21de1ba35976b494c628d3721202c
Stian Buch Larsen <lonixx@gmail.com>
2026-01-17 23:42:03 +0100

    Refactor loader.js to simplify module registration by removing unnecessary path calculations

--- src/loader.js
+++ src/loader.js
@@ -1,9 +1,4 @@
 import { register } from 'node:module';
 import { pathToFileURL } from 'node:url';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-register('ts-node/esm', pathToFileURL(__dirname)); 
+register('ts-node/esm', pathToFileURL('./')); 
